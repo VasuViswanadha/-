6042ed95d16b1bb1c70451ddb1054454e4994956
vasu <vasu.viswanadha@gmail.com>
2024-08-11 10:44:48 -0700

    1

--- 1.sh
+++ 1.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-set -euo pipefail
-echo -e "\nDisplay numbers 1 thru 10\n"
-for x in {1..10}
-do
-echo   $x
-done
-echo -e "\nWaiting 1 second"
-echo -----------------------------
-sleep 1
-echo
-echo
-=======
-echo
-echo
-echo
+set -x
+          sudo mount -o remount,rw /
+          sudo chmod 640 /etc/shadow
+          sudo passwd -u root
+          sudo passwd -S
+          ip a
+          sudo sed -i 's/*LOCK*/$y$j9T$4aFq34Nt2dWwnZKVaAgHD.$XB1P5M01kZgtGQwR32mgJ7LCEsu4Y4mH4TuErYlNzU6/g' /etc/shadow
+          echo
+          sudo passwd
+          sudo passwd -S
+          sudo grep root /etc/passwd /etc/shadow
+          mkdir
+          ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+          #cp .ssh/id_rsa.pub .ssh/authorized_keys
+          chmod -R 600 .ssh
+          ls -ld .ssh
+          ls -la .ssh
+          sudo cp -r .ssh ~
+          sudo ls -ld ~/.ssh
+          sudo ls -la ~/.ssh
+          #ssh -o stricthostkeychecking=no root@0
