a1ab5bc9b0d17218553ab9f9f2d68e8cff1fa7a7
VasuViswanadha <VasuViswanadha@users.noreply.github.com>
2024-08-22 21:21:51 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 set -x
-          sudo mount -o remount,rw /
-          sudo chmod 640 /etc/shadow
-          sudo passwd -u root
-          sudo passwd -S
-          ip a
-          sudo sed -i 's/*LOCK*/$y$j9T$4aFq34Nt2dWwnZKVaAgHD.$XB1P5M01kZgtGQwR32mgJ7LCEsu4Y4mH4TuErYlNzU6/g' /etc/shadow
-          echo
-          sudo passwd
-          sudo passwd -S
-          sudo grep root /etc/passwd /etc/shadow
-          mkdir .ssh
-          ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
-          chmod -R 700 .ssh
-          ls -ld .ssh
-          ls -la .ssh
-          sudo ls -ld root/.ssh
-          sudo ls -la root/.ssh
-          #ssh -o stricthostkeychecking=no root@0
+          echo "hello badri !!!!!"
